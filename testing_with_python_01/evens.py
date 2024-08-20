950b968bb893e800b93f27bca711553fdb5bda6f
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-20 18:17:08 +0000

    trying to follow the LMS

--- testing_with_python_01/evens.py
+++ testing_with_python_01/evens.py
@@ -1,13 +1,4 @@
-def even_number_of_evens(numbers):
-    """
-    Should Raise a TypeError if a list in not passed into the function
-    error message: "A list was not passed into the function"
-    if the list is empty it will return False
-    if the number of even numbers is odd - return False
-    if the numner of even numbers is even - return True
-    """
-
+def even_number_of_evens(numbvers):
     return None
 
-if __name__ == "__main__":
-    print(even_number_of_evens(5))
+print(even_number_of_evens(5))
--- testing_with_python_01/test_evens.py
+++ testing_with_python_01/test_evens.py
@@ -1,10 +1,5 @@
 import unittest
-from evens import even_number_of_evens
 
+class TestEvens(unittest.TestCase)
+pass
 
-class TestEvens(unittest.TestCase):
-    pass
-
-
-if __name__ == "__main__":
-    unittest.main()
