0dfe0ba0b53b20a5f4dbca0db796cfb181fdd744
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-20 18:31:54 +0000

    i tried to correct the spelling errors but it's still not working properly, has to be something else and I'm continuing my search

--- testing_with_python_01/test_evens.py
+++ testing_with_python_01/test_evens.py
@@ -1,5 +1,8 @@
 import unittest
+from evens import even_number_of_evens
 
-class TestEvens(unittest.TestCase)
-pass
+class TestEvens(unittest.TestCase):
+    pass  # You'll add your test methods here later
 
+if __name__ == '__main__':
+    unittest.main()
