1820640b24dacfe5e378ec5f06b107016fc9013a
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-21 17:11:11 +0000

    have to make a commit

--- evens.py
+++ evens.py
@@ -7,21 +7,17 @@ def even_number_of_evens(numbers):
     if the numner of even numbers is even - return True
     """
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            evens = 0
-            
-        for n in numbers:
-            if n % 2 == 0:
-                evens += 1
-        if evens:
-            return evens % 2 == 0
-        else:
-            return False
+        evens = sum([1 for n in numbers if n % 2 == 0])
+
+        return True if evens and evens % 2 == 0 else False
+
+    #    if evens:
+    #       return evens % 2 == 0
+    #    else:
+    #        return False
     else:
         raise TypeError("A list was not passed into the function")
     return None
 
 if __name__ == "__main__":
-    print(even_number_of_evens(5))
+    even_number_of_evens([2, 1, 4])
